cd19b1030453cab60e469648c72099a3e4c4d064
fanlun007 <13058100289@163.com>
2018-09-16 20:30:01 +0800

    1

--- src/main/java/com/zk/srs/controller/ScoreController.java
+++ src/main/java/com/zk/srs/controller/ScoreController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -19,25 +20,70 @@ import java.util.List;
 public class ScoreController {
 
     @GetMapping("/scoreSelf/{sno}")
-    public String toscoreListPage(@PathVariable("sno") Integer sno, Model score_list){
+    public String toscoreListPage(@PathVariable("sno") Integer sno, Model score_list) {
         String scos_json = FileUtils.readFile(Storage.SCORE_PATH);
         List<Score> allScore = JSON.parseArray(scos_json, Score.class);
 
         List<Score> scoWithSno = new ArrayList<>();
-        for (Score score : allScore){
+        for (Score score : allScore) {
             if (score.getSno() == sno) {
                 scoWithSno.add(score);
             }
         }
-        score_list.addAttribute("score_list", scoWithSno);
+        if (scoWithSno.size() != 0) {
+            score_list.addAttribute("score_list", scoWithSno);
+        }else{
+            score_list.addAttribute("score_list", null);
+        }
         return "scoreList";
     }
 
     @GetMapping("/editScore")
-    public void editScore(HttpServletRequest request){
+    public String editScore(HttpServletRequest request, Model model) {
         String sno = request.getParameter("sno");
         String cn = request.getParameter("cn");
-
+        String scos_json = FileUtils.readFile(Storage.SCORE_PATH);
+        List<Score> allScore = JSON.parseArray(scos_json, Score.class);
+        Score edit_score = null;
+        for (Score score : allScore) {
+            if (score.getSno() == Integer.parseInt(sno) && score.getCn().equals(cn)) {
+                edit_score = score;
+            }
+        }
+        model.addAttribute("editScore", edit_score);
+        return "editScorePage";
     }
 
+    @PutMapping("/score")
+    public String modifyScore(Score mdfScore){
+        String scos_json = FileUtils.readFile(Storage.SCORE_PATH);
+        List<Score> allScore = JSON.parseArray(scos_json, Score.class);
+
+        for (int i = 0; i< allScore.size(); i++){
+            if (allScore.get(i).getSno() == mdfScore.getSno() && allScore.get(i).getCn().equals(mdfScore.getCn())) {
+                allScore.remove(i);
+                allScore.add(i, mdfScore);
+            }
+        }
+        String jsonUpdate = JSON.toJSONString(allScore);
+        FileUtils.writeFile(Storage.SCORE_PATH, jsonUpdate);
+        return "redirect:/scoreSelf/" + mdfScore.getSno();
+    }
+
+    @GetMapping("/deleteScore")
+    public String deleteScore(HttpServletRequest request){
+        String sno = request.getParameter("sno");
+        String cn = request.getParameter("cn");
+        String scos_json = FileUtils.readFile(Storage.SCORE_PATH);
+        List<Score> allScore = JSON.parseArray(scos_json, Score.class);
+
+        for (int i = 0; i<allScore.size();i++){
+            if (allScore.get(i).getSno() == Integer.parseInt(sno) && allScore.get(i).getCn().equals(cn)) {
+                allScore.remove(i);
+            }
+        }
+        String deleteJson = JSON.toJSONString(allScore);
+        FileUtils.writeFile(Storage.SCORE_PATH, deleteJson);
+        return "redirect:/scoreSelf/" + sno;
+    }
 }
